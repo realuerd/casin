fbfadbf08c3b077d4024b0d0234809a71046d4b6
realuerd <paypaluerd@gmail.com>
2024-07-25 01:46:15 +0200

    Update sql.php

--- core/sql.php
+++ core/sql.php
@@ -3,13 +3,13 @@
 		// Database for my localhost
 		define("DB_HOST", "localhost"); 	// MySQL Database Host
 		define("DB_USER", "root");			// MySQL Username
-		define("DB_PASS", "");  			// MySQL Password
-		define("DB_NAME", "");  	// Database Name
+		define("DB_PASS", "testt");  			// MySQL Password
+		define("DB_NAME", "etronix");  	// Database Name
 	} else {
 		// Database for public
 		define("DB_HOST", "localhost"); 	// MySQL Database Host
-		define("DB_USER", "replace_user");			// MySQL Username
-		define("DB_PASS", "replace_password");  	// MySQL Password
-		define("DB_NAME", "replace_db");  		// Database Name
+		define("DB_USER", "test");			// MySQL Username
+		define("DB_PASS", "testt");  	// MySQL Password
+		define("DB_NAME", "etronix");  		// Database Name
 	}
 ?>
